5ffad64944a491f50661e597f56dd7d8119f733c
tolaoner <59030417+tolaoner@users.noreply.github.com>
2019-12-19 16:07:06 +0100

    add __str__ function

--- bankaccount.py
+++ bankaccount.py
@@ -5,17 +5,20 @@ class Account:
 		self.balance=balance
 	def deposit(self,deposit_amount):
 		self.balance=self.balance+deposit_amount
-		print('New balance: ',self.balance)
+		print('Deposit accepted!\nNew balance: ',self.balance)
 	def withdraw(self,withdraw_amount):
 		if withdraw_amount>self.balance:
 			print("You don't have enough money in your account. Your balance: ",self.balance)
 		else:
 			self.balance = self.balance - withdraw_amount
-			print('New Balance: ', self.balance)
+			print('Withdrawal Accepted!\nNew Balance: ', self.balance)
 		#may not exceed the available balance	
+	def __str__(self):
+		return f'Account owner: {self.owner}\nAccount balance:{self.balance}'
 asd = Account('Oner',500)
 asd.deposit(300)
 asd.deposit(200)
 asd.withdraw(1100)
 asd.withdraw(500)
 asd.withdraw(200)
+print(asd)
